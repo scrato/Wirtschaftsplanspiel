13e13369f5d9fba31599eb838ed3ed948ce864b8
Your Name <Mickaello2003@gmx.de>
2012-01-11 10:59:29 +0100

    Prüfmethoden in Production, Ausführung in CompanyController

--- Wirtschaftsplanspiel/src/Client/Entities/Production.java
+++ Wirtschaftsplanspiel/src/Client/Entities/Production.java
@@ -1,12 +1,53 @@
 package Client.Entities;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Dictionary;
 
+import Client.Application.NotEnoughMachinesException;
+import Client.Application.NotEnoughPersonalException;
+import Client.Application.NotEnoughRessourcesException;
 import Client.Entities.Ressource.RessourceType;
 
 public class Production {
+    private double pricePerUnit;
+	public void enoughRessources(int units) throws NotEnoughRessourcesException
+	{
+			Company comp = Company.getInstance();
+		   Dictionary<RessourceType, Ressource> ressources = comp.getAllRessources();
+		   NotEnoughRessourcesException resExc = new NotEnoughRessourcesException();
+		   while(ressources.elements().hasMoreElements()){
+			   Ressource res = ressources.elements().nextElement();
+				//MissingUnits sind die Einheiten, die nicht produziert werden können, weil Maschinen fehlen.
+			   int missingunits = (Ressource.getNeed(res.getType())* units)  - res.getAvailableUnits();
+			   if (missingunits > 0)
+				   resExc.addNewRessource(res.getType(), missingunits);
+		   }
+		   if(resExc.isFilled())
+			   throw resExc;
+	}
    
-   
+	public void enoughMachines(int units) throws NotEnoughMachinesException
+	{
+		Company comp = Company.getInstance();
+		NotEnoughMachinesException  macExc = new NotEnoughMachinesException();
+	   	for(MachineType type: MachineType.values()){
+	   		
+	   		//MissingUnits sind die Einheiten, die nicht produziert werden können, weil Maschinen fehlen.
+	   		int missingunits = units - comp.getMachineCapacity(type);
+	   		if (missingunits > 0)
+	   			macExc.AddMachine(type, missingunits);
+	   	}
+	   	if(macExc.isFilled())
+	   		throw macExc;		
+	}
  
+	public void enoughPersonal(int units) throws NotEnoughPersonalException
+	{
+		//TODO: Prüfen, ob genug Personal für die Produktion vorhanden ist
+		NotEnoughPersonalException perExc = new NotEnoughPersonalException();
+		throw new UnsupportedOperationException("Noch nicht implementiert");
+	}
+	
+	public void setPricePerUnit(double price){
+		this.pricePerUnit = price;
+	}
 }
