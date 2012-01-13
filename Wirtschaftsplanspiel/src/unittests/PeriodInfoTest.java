3df64cb253b7ec910f83bc4a75f1612e03cf5d7f
Your Name <Mickaello2003@gmx.de>
2012-01-13 23:13:36 +0100

    Weiteres deBugging

--- Wirtschaftsplanspiel/src/unittests/PeriodInfoTest.java
+++ Wirtschaftsplanspiel/src/unittests/PeriodInfoTest.java
@@ -15,6 +15,7 @@ import Client.Entities.Employee;
 import Client.Entities.EmployeeType;
 import Client.Entities.Machine;
 import Client.Entities.MachineType;
+import Client.Entities.PeriodInfo.Balance;
 import Client.Entities.PeriodInfo.GuV;
 import Client.Entities.Ressource.RessourceType;
 
@@ -25,13 +26,14 @@ import Client.Entities.Ressource.RessourceType;
 public class PeriodInfoTest extends TestCase  {
 	private Company comp;
 	private GuV g;
+	private Balance b;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
 		comp = Company.getInstance();
-		comp.incMoney(40000000);
+		comp.incMoney(400000);
 		CompanyController.takeCredit(40000, 5);
 		CompanyController.initRessource(RessourceType.Stockfisch, 100, 1.0);
 		CompanyController.initRessource(RessourceType.Verpackungsmaterial, 10, 10);
@@ -53,10 +55,38 @@ public class PeriodInfoTest extends TestCase  {
 		CompanyController.produce();
 	}
 	
-	public void testGuV(){
+	public void testPeriodInfo(){
 		g = comp.getPeriodInfo().getGuV();
 		Assert.assertEquals(0.0, g.changeInStockRessources);
 		Assert.assertEquals(200.0, g.changeInStockFinishedProducts);
+		 System.out.println("BV Endprodukte: " + g.changeInStockFinishedProducts);
+		 System.out.println("BV Ressourcen: " + g.changeInStockRessources);
+		 System.out.println("Abschreibung: " + g.deprecation);
+		 System.out.println("Kosten für Feuerung MA: " + g.employeeDismissalCosts);
+		 System.out.println("Kosten für Einstellung MA: " + g.employeeHiringCosts);
+		 System.out.println("Unternehmerlohn: " + g.employerSallery);
+		 System.out.println("Zinsaufwand: " + g.interest);
+		 System.out.println("Miete: " + g.rental);
+		 System.out.println("Ressourcenaufwand: " + g.ressourceCost);
+		 System.out.println("Löhne und Gehälter: " + g.wages);
+
+		 b = comp.getPeriodInfo().getBalance();
+		 System.out.println("Bank: " + b.bank);
+		 System.out.println("Verbindlichkeiten: " + b.credit);
+		 System.out.println("Eigenkapital: " + b.equity);
+		 System.out.println("Maschinen: " + b.machineValue);
+		 System.out.println("Ressourcen: " + b.ressourceValue);
+		 System.out.println("Gesamteinnahmen: " + b.totallyearned);
+		 System.out.println("Gesamtausgaben: " + b.totallypaid);
+	}
+
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@org.junit.After
+	public void tearDown() throws Exception {
+		comp.setCredit(null);
 	}
 
 }
