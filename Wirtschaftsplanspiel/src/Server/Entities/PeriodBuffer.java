dc5733634dc9a0dba91791a197fd1599ee749392
Lars Baumann <lbaumann-lfs@t-online.de>
2012-01-17 13:12:52 +0100

    asdasdasd

--- Wirtschaftsplanspiel/src/Server/Entities/PeriodBuffer.java
+++ Wirtschaftsplanspiel/src/Server/Entities/PeriodBuffer.java
@@ -8,5 +8,12 @@ import Server.Entities.Disposal;
 public class PeriodBuffer {
 	
 	public static Map<Integer, Disposal> Disposals = new TreeMap<Integer, Disposal>();
+	public static int getTotalDisposal () {
+		int total = 0;
+		for (Disposal dis : Disposals.values()) {
+			total += dis.quantity;
+		}
+		return total;
+	}
 	
 }
